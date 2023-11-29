e005eb92e70702c55cff0fdfa7cb51b861c9285e
Gabor Cselle <gaborcselle@users.noreply.github.com>
2023-11-29 11:40:17 -0800

    Plug in a small finetuned "write my posts" model

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -16,7 +16,7 @@ export async function POST(req: Request) {
 
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
+    model: 'ft:gpt-3.5-turbo-0613:personal:wmp-no-replies:8QKbNwAj',
     stream: true,
     messages: messages,
   });
